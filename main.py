42b0b98042dc230ab1feffe7034a43e19be60b5a
b10t <b10t.rus@gmail.com>
2022-03-23 15:30:38 +0800

    Download books from tululu.org.

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-import requests
+import os
 
+import requests
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
 
 if __name__ == '__main__':
-    pass
+    urllib3.disable_warnings(InsecureRequestWarning)
+
+    url = "https://tululu.org/txt.php"
+
+    books_path = 'books'
+    os.makedirs(books_path, exist_ok=True)
+
+    for book_id in range(1, 11):
+
+        params = {
+            'id': book_id,
+        }
+
+        response = requests.get(url, verify=False, params=params)
+        response.raise_for_status()
+
+        filename = f'id{book_id}.txt'
+        with open(os.path.join(books_path, filename), 'wb') as file:
+            file.write(response.content)
