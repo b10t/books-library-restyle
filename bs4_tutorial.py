ffd793f406ca580b7bf87314d6a48166b17e7a0f
b10t <b10t.rus@gmail.com>
2022-04-21 16:58:27 +0800

    Получение ссылки на книгу со страницы.

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -1,19 +1,55 @@
+from urllib.parse import urljoin
+
 import requests
 from bs4 import BeautifulSoup
 
+# url = 'https://tululu.org/b7/'
+# response = requests.get(url)
+# response.raise_for_status()
 
-url = 'https://tululu.org/b7/'
+# soup = BeautifulSoup(response.text, 'lxml')
+
+# title_tag = soup.find('h1')
+
+# book_author, book_title = [text.strip() for text in title_tag.text.split('::')]
+
+# print(book_author, book_title)
+
+
+# url = 'https://tululu.org/b7/'
+# response = requests.get(url)
+# response.raise_for_status()
+
+# soup = BeautifulSoup(response.text, 'lxml')
+
+# book_image_src = soup.find('div', class_='bookimage').find('img')['src']
+
+# print(urljoin(url, book_image_src))
+
+
+url = 'https://tululu.org/b9/'
 response = requests.get(url)
 response.raise_for_status()
 
 soup = BeautifulSoup(response.text, 'lxml')
 
-title_tag = soup.find('h1')
+download_tag = soup.find('a', string='скачать txt')
+download_url = download_tag['href'] if download_tag else None
 
-book_author, book_title = [text.strip() for text in title_tag.text.split('::')]
+print(download_url)
 
-print(book_author, book_title)
-# print(*[text.strip() for text in title_tag.text.split('::')], sep='\n')
+book_url = soup.find('table', class_='d_book').find_all('a')
+
+# print(urljoin(url, book_image_src))
+# print(*book_url)
+
+
+# title_tag = soup.find('h1')
+
+# book_author, book_title = [text.strip() for text in title_tag.text.split('::')]
+
+# print(book_author, book_title)
+
 
 # print(soup.find('img', class_='attachment-post-image')['src'])
 
