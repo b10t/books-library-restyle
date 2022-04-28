5b902acac74a2862bf3dd765b1426fc7a196ac90
b10t <b10t.rus@gmail.com>
2022-04-28 14:35:47 +0800

    Dried the code.

--- main.py
+++ main.py
@@ -127,4 +127,3 @@ if __name__ == '__main__':
             download_image(book_image_url, image_filename)
         except requests.HTTPError or requests.ConnectionError:
             print('Не удалось скачать книгу с сервера.')
-            continue
