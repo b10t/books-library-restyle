e6deea9eda31815b070d0303275c10a408de2e88
b10t <b10t.rus@gmail.com>
2022-05-29 18:54:40 +0800

    Add paginator.

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 import urllib.parse
 
@@ -14,9 +15,10 @@ def on_reload():
     books_catalog = json.loads(books_json)
 
     for book in books_catalog:
-        book['image_filename'] = os.path.join('../images', book['image_filename'])
+        book['image_filename'] = os.path.join(
+            '/../images', book['image_filename'])
         book['book_filename'] = urllib.parse.quote(
-            os.path.join('../books', book['book_filename'])
+            os.path.join('/../books', book['book_filename'])
         )
 
     env = Environment(
@@ -24,6 +26,8 @@ def on_reload():
         autoescape=select_autoescape(['html'])
     )
     template = env.get_template('template.html')
+
+    pages_number = math.ceil(len(books_catalog) / 20)
 
     for page_index, books in enumerate(chunked(books_catalog, 20)):
         page_save_path = os.path.join(
@@ -33,6 +37,8 @@ def on_reload():
 
         rendered_page = template.render(
             books_catalog=list(chunked(books, 2)),
+            pages_number=pages_number + 1,
+            page_index=page_index + 1,
         )
 
         with open(page_save_path, 'w', encoding="utf8") as file:
@@ -45,4 +51,4 @@ on_reload()
 
 server = Server()
 server.watch('template.html', on_reload)
-server.serve(root='.')
+server.serve(root='.', default_filename='./pages/index1.html')
